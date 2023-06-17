617ceb32dbd5e69a4a1ba57862d0758edc406b97
AlanGP2001 <93557645+AlanGP2001@users.noreply.github.com>
2023-06-17 16:07:59 -0600

    Ver productos para usuarios

--- src/main/java/mx/edu/uteq/controller/AdministradorController.java
+++ src/main/java/mx/edu/uteq/controller/AdministradorController.java
@@ -1,22 +1,23 @@
 package mx.edu.uteq.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import mx.edu.uteq.model.Producto;
+import mx.edu.uteq.service.ProductoService;
+
 @Controller
 @RequestMapping("/administrador")
-public class AdministradoController {
-
-	@GetMapping("")
-	public String home() {
-		return "administrador/home";
-	}
-	
-	/*
+public class AdministradorController {
 	@Autowired
 	private ProductoService productoService;
 	
+	/*
 	@Autowired
 	private IUsuarioService usuarioService;
 	
@@ -24,17 +25,15 @@ public class AdministradoController {
 	private IOrdenService ordensService;
 	
 	private Logger logg= LoggerFactory.getLogger(AdministradorController.class);
-
+	*/
 	@GetMapping("")
 	public String home(Model model) {
 
 		List<Producto> productos = productoService.findAll();
 		model.addAttribute("productos", productos);
-
-
 		return "administrador/home";
 	}
-	
+	/*
 	@GetMapping("/usuarios")
 	public String usuarios(Model model) {
 		model.addAttribute("usuarios", usuarioService.findAll());
--- src/main/java/mx/edu/uteq/controller/ProductoController.java
+++ src/main/java/mx/edu/uteq/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package mx.edu.uteq.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import mx.edu.uteq.model.Producto;
 import mx.edu.uteq.model.Usuario;
 import mx.edu.uteq.service.ProductoService;
+import mx.edu.uteq.service.UploadFileService;
 
 @Controller
 @RequestMapping("/productos")
@@ -27,9 +31,9 @@ public class ProductoController {
 	@Autowired
 	private IUsuarioService usuarioService;
 	
+	 */
 	@Autowired
 	private UploadFileService upload;
-	 */
 	
 	@GetMapping("")
 	public String show(Model model) {
@@ -43,23 +47,23 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/save")
-	public String save(Producto producto /*, @RequestParam("img") MultipartFile file, HttpSession session*/) /*throws IOException*/ {
+	public String save(Producto producto, @RequestParam("img") MultipartFile file/*, HttpSession session*/) throws IOException {
 		LOGGER.info("Este es el objeto producto {}",producto);
 		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);	
 		
 		/*
 		Usuario u= usuarioService.findById(Integer.parseInt(session.getAttribute("idusuario").toString() )).get();
+		 */
 		
 		//imagen
 		if (producto.getId()==null) { // cuando se crea un producto
-			String nombreImagen= upload.saveImage(file);
+			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}else {
 			
 		}
 		
-		 */
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
@@ -77,26 +81,25 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/update")
-	public String update(Producto producto/*, @RequestParam("img") MultipartFile file */) /*throws IOException */{
-		/*
-		Producto p =  new Producto();
-		p=productoService.get(producto.getId()).get();
+	public String update(Producto producto, @RequestParam("img") MultipartFile file ) throws IOException {
+		
+		Producto p = new Producto();
+		p = productoService.get(producto.getId()).get();
 		
 		if (file.isEmpty()) {
 			// editamos el producto pero no cambiamos la imagem
-			
 			producto.setImagen(p.getImagen());
 		} else {
-			// cuando se edita tbn la imagen			
+			// cuando se edita tambien la imagen			
 			//eliminar cuando no sea la imagen por defecto
 			if (!p.getImagen().equals("default.jpg")) {
 				upload.deleteImage(p.getImagen());
 			}
-			String nombreImagen= upload.saveImage(file);
+			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}
 		producto.setUsuario(p.getUsuario());
-		 */
+		 
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
@@ -104,16 +107,13 @@ public class ProductoController {
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
 		
-		/*
 		Producto p = new Producto();
-		p=productoService.get(id).get();
+		p = productoService.get(id).get();
 		
 		//eliminar cuando no sea la imagen por defecto
 		if (!p.getImagen().equals("default.jpg")) {
 			upload.deleteImage(p.getImagen());
 		}
-		 */
-		
 		productoService.delete(id);
 		return "redirect:/productos";
 	}
