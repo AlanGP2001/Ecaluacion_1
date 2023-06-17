45d307b946a05ae7626b4b4bd1dbda20510350f5
AlanGP2001 <93557645+AlanGP2001@users.noreply.github.com>
2023-06-17 16:21:18 -0600

    Ver detalle del Producto

--- src/main/java/mx/edu/uteq/controller/HomeController.java
+++ src/main/java/mx/edu/uteq/controller/HomeController.java
@@ -4,16 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import mx.edu.uteq.model.Producto;
 import mx.edu.uteq.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	/*
+	
 	private final Logger log = LoggerFactory.getLogger(HomeController.class);
-	*/
+	
 	@Autowired
 	private ProductoService productoService;
 	/*
@@ -45,7 +52,7 @@ public class HomeController {
 		*/
 		return "usuario/home";
 	}
-	/*
+	
 	@GetMapping("productohome/{id}")
 	public String productoHome(@PathVariable Integer id, Model model) {
 		log.info("Id producto enviado como parámetro {}", id);
@@ -54,10 +61,11 @@ public class HomeController {
 		producto = productoOptional.get();
 
 		model.addAttribute("producto", producto);
-
+		
 		return "usuario/productohome";
 	}
 
+	/*
 	@PostMapping("/cart")
 	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
 		DetalleOrden detalleOrden = new DetalleOrden();
